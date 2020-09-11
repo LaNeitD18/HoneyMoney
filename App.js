3e752b6634c470affd6d3dbfe8f4e98e02649b3c
hau28 <muiheoconghau@gmail.com>
2020-09-11 09:30:45 +0700

    Minor changes AddTransactionScreen CategoriesScreen

--- App.js
+++ App.js
@@ -1,30 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import {NavigationContainer } from '@react-navigation/native'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createStackNavigator} from '@react-navigation/stack'
-import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import WalletScreen from './screens/WalletScreen'
-import CategoriesScreen from './screens/CategoriesScreen'
-
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import WalletScreen from "./screens/WalletScreen";
+import CategoriesScreen from "./screens/CategoriesScreen";
 
 //Redux
-import {createStore} from 'redux';
-import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import allReducers from './reducers/allReducers';
+import allReducers from "./reducers/allReducers";
 
 //import WalletContainer from './containers/WalletContainer'
-import AddTransactionScreen from './screens/AddTransactionScreen';
-import AddWalletScreen from './screens/AddWalletScreen';
-import EditCategoryScreen from './screens/EditCategoryScreen';
-import AddCategoryScreen from './screens/AddCategoryScreen';
-import Calculator from './components/Calculator';
+import AddTransactionScreen from "./screens/AddTransactionScreen";
+import AddWalletScreen from "./screens/AddWalletScreen";
+import EditCategoryScreen from "./screens/EditCategoryScreen";
+import AddCategoryScreen from "./screens/AddCategoryScreen";
+import Calculator from "./components/Calculator";
 
-import WalletNavigator from './screens/WalletNavigator';
+import WalletNavigator from "./screens/WalletNavigator";
 
-import CategoryNavigator from './screens/CategoryNavigator';
+import CategoryNavigator from "./screens/CategoryNavigator";
 
 //Navigator
 const Tab = createBottomTabNavigator();
@@ -34,18 +33,14 @@ const Stack = createStackNavigator();
 let store = createStore(allReducers);
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Tab.Navigator tabBarOptions={{ activeTintColor: 'blue', keyboardHidesTabBar: true}}>
-          <Tab.Screen name="Trang chủ" component={WalletNavigator} options={{title: 'Wallet'}} />
-          <Tab.Screen name="Danh mục" component={CategoryNavigator} options={{title: 'Categories'}}/>
-          <Tab.Screen name="AddTrans" component={AddTransactionScreen} options={{title: 'Thêm giao dịch'}}/>
-          <Tab.Screen name="AddCate" component={AddCategoryScreen} options={{title: 'Thêm Categories'}}/>
-          <Tab.Screen name="AddWallet" component={AddWalletScreen} options={{title: 'Thêm ví'}}/>
-          <Tab.Screen name="EditCate" component={EditCategoryScreen} options={{title: 'Sửa Categories'}}/>
-        </Tab.Navigator>
-      </NavigationContainer>
-    </Provider>
-  )
+    return (
+        <Provider store={store}>
+            <NavigationContainer>
+                <Tab.Navigator tabBarOptions={{ activeTintColor: "blue", keyboardHidesTabBar: true }}>
+                    <Tab.Screen name="ATS" component={AddTransactionScreen} options={{ title: "ATS" }} />
+                    <Tab.Screen name="CS" component={CategoriesScreen} options={{ title: "CS" }} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </Provider>
+    );
 }
