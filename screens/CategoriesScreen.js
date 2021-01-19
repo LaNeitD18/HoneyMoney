16e1921643b3e0f3c9eaa4fc35c6687f62956409
hau28 <muiheoconghau@gmail.com>
2021-01-19 08:58:50 +0700

    Small fix

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -258,10 +258,6 @@ class CategoriesScreen extends React.Component {
                     <CategoryTable rows={rows} />
                     <Divider />
                 </ScrollView>
-
-                <Animated.Text style={{ opacity: opacityValue }}>
-                    I fade in when you scroll down
-                </Animated.Text>
             </View>
         );
     }
